f9e49314d375ebb061a3a919acead7b1ac0c0fc7
Denis Lafin <dslf@live.ru>
2024-11-27 23:55:20 +0300

    test

--- engine/build.bat
+++ engine/build.bat
@@ -1,20 +1,17 @@
 REM Build script for engine
-@Echo off
+@ECHO OFF
 SetLocal EnableDelayedExpansion
-
 REM Get a list of all the .c files.
-FOR /r %%f in (*.c) do (
+SET cFilenames=
+FOR /R %%f in (*.c) do (
     SET cFilenames=!cFilenames! %%f
 )
-
-REM echo Files
-
+REM echo "Files:" %cFilenames%
 SET assembly=engine
 SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
-
+REM -Wall -Werror
 SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
 SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
 SET defines=-D_DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS
-
-ECHO "bUILDING %ASSEMBLY%%..."
-clang %cFilenames% %compilerFlags% -o ../bin/%assembly.dll %defines% %includeFlags% %linkerFlags%
+ECHO "Building %assembly%%..."
+clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
