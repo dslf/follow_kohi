0fceb740bed6292bd3d297ae785dab54e48bf093
Denis Lafin <dslf@live.ru>
2024-11-28 00:37:56 +0300

    003-1

--- engine/src/core/logger.h
+++ engine/src/core/logger.h
@@ -7,6 +7,11 @@
 #define LOG_DEBUG_ENABLED 1
 #define LOG_TRACE_ENABLED 1
 
+#if KRELEASE == 1
+#define LOG_DEBUG_ENABLED 0
+#define LOG_TRACE_ENABLED 0
+#endif
+
 typedef enum log_level {
     LOG_LEVEL_FATAL = 0,
     LOG_LEVEL_ERROR = 1,
@@ -15,3 +20,11 @@ typedef enum log_level {
     LOG_LEVEL_DEBUG = 4,
     LOG_LEVEL_TRACE = 5
 } log_level;
+
+b8 initialiaze_logging();
+void shutdown_logging();
+
+KAPI void log_output(log_level level, const char* message, ...);
+
+// Logs a fatel-level messages.
+#define KFATAL(message, ...) log_output(LOG_LEVEL_FATAL, message, ##__VA_ARGS__);
